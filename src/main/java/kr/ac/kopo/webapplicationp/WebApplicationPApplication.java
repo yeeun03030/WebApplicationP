b18b0923aa16900fcdb69b9a7238bbc87432ede8
yeeun03030 <skdPdms04@gmail.com>
2024-03-28 11:54:08 +0900

    Web

---
+++ src/main/java/kr/ac/kopo/webapplicationp/WebApplicationPApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.webapplicationp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplicationPApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplicationPApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/kopo/webapplicationp/WebApplicationPApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.webapplicationp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationPApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
