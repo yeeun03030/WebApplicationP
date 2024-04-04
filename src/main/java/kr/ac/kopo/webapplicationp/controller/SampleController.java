aebaa28064136aafed7ae2a2243ff08b48019863
yeeun03030 <skdPdms04@gmail.com>
2024-04-04 11:53:51 +0900

    20240404_SampleController, SampleDTO

---
+++ src/main/java/kr/ac/kopo/webapplicationp/controller/SampleController.java
@@ -0,0 +1,37 @@
+package kr.ac.kopo.webapplicationp.controller;
+
+import kr.ac.kopo.webapplicationp.dto.SampleDTO;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Controller
+@RequestMapping("/sample")
+public class SampleController {
+
+    @GetMapping("/ex1")
+    public void ex() {
+    }
+
+    @GetMapping({"/ex2", "/ex2_1"})
+    public void exModel(Model model) {
+        List<SampleDTO> list = IntStream.rangeClosed(1, 20).asLongStream().mapToObj(i -> {
+            SampleDTO dto = SampleDTO.builder()
+                    .sno(i)
+                    .first("First.." + i)
+                    .last("Last.." + i)
+                    .regTime(LocalDateTime.now())
+                    .build();
+            return dto;
+        }).collect(Collectors.toList());
+
+        model.addAttribute("list", list);
+    }
+}
---
+++ src/main/java/kr/ac/kopo/webapplicationp/dto/SampleDTO.java
@@ -0,0 +1,15 @@
+package kr.ac.kopo.webapplicationp.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder(toBuilder = true)
+public class SampleDTO {
+    private Long sno;
+    private String first;
+    private String last;
+    private LocalDateTime regTime;
+}
