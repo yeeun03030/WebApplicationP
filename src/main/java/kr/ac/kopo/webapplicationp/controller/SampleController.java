dc02b24e19cc4a107ec0755491e3f263562fa58d
yeeun03030 <skdPdms04@gmail.com>
2024-04-11 11:20:15 +0900

    타임리프 Inline(JS), Block(반복문), Hyper Link 처리, numbers, temporals(숫자, 날짜 형식 지정)

--- src/main/java/kr/ac/kopo/webapplicationp/controller/SampleController.java
+++ src/main/java/kr/ac/kopo/webapplicationp/controller/SampleController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -20,7 +21,7 @@ public class SampleController {
     public void ex() {
     }
 
-    @GetMapping({"/ex2", "/ex2_1"})
+    @GetMapping({"/ex2", "/ex2_1", "/exBlock", "exLink"})
     public void exModel(Model model) {
         List<SampleDTO> list = IntStream.rangeClosed(1, 20).asLongStream().mapToObj(i -> {
             SampleDTO dto = SampleDTO.builder()
@@ -34,4 +35,24 @@ public class SampleController {
 
         model.addAttribute("list", list);
     }
+
+    @GetMapping("/exInline")
+    public String exInline(RedirectAttributes redirectAttributes) {
+        SampleDTO dto = SampleDTO.builder()
+                .sno(100L)
+                .first("First..100")
+                .last("Last..100")
+                .regTime(LocalDateTime.now())
+                .build();
+        redirectAttributes.addFlashAttribute("result", "Success");
+        redirectAttributes.addFlashAttribute("dto", dto);
+
+        return "redirect:/sample/ex3";
+    }
+
+    @GetMapping("/ex3")
+    public void ex3() {
+
+    }
+
 }
